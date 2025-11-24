b95beedbe9d151326a8d6a8087e6de748d63132e
JeffreyBarrios <jeffreybarrios629@gmail.com>
2025-11-23 18:24:16 -0600

    Reajuste a la ruta controller

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IMSA_Solicitud_Service.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[Action]")]
     [ApiController]
     public class CatalogoController : ControllerBase
     {
