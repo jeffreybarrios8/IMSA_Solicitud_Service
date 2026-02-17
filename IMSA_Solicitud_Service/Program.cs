cdf1520951662fa2ce2beb590fa0d3f5d175f2c0
JeffreyBarrios <jeffreybarrios629@gmail.com>
2026-02-16 18:01:58 -0600

    allow url front

--- IMSA_Solicitud_Service/Program.cs
+++ IMSA_Solicitud_Service/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularDev",
         policy => policy
-            .WithOrigins("http://localhost:4200") // Angular dev server
+            .WithOrigins("http://imsafrontend01-001-site1.jtempurl.com") // Angular dev server
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
